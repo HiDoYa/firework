da406a4e7d915e1754ec942f0e471131f4584e95
HiDoYa <hiroyagojo@gmail.com>
2018-08-11 21:19:01 -0700

    Fixed seg fault on leaving

--- main.cpp
+++ main.cpp
@@ -31,6 +31,7 @@ int main() {
 			switch (event.type) {
 				case sf::Event::Closed:
 					window.close();
+					instr.setString("");
 					break;
 				case sf::Event::KeyPressed:
 					addFireworks(fr, done);
