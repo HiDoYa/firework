2b8c5334892952f61a7902e87c8da3afac34814c
HiDoYa <hiroyagojo@gmail.com>
2018-08-12 11:35:11 -0700

    Added toggle for autofire

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 #include <iostream>
 #include "include/firework.h"
 
-void addFireworks(std::vector<std::shared_ptr<Firework>>&, std::vector<bool>&);
+void addFireworks(std::vector<std::shared_ptr<Firework>>&, std::vector<bool>&, bool&, sf::Text&);
+void fire(std::vector<std::shared_ptr<Firework>>&, std::vector<bool>&);
 
 int main() {
 	srand(time(NULL));
@@ -14,6 +15,7 @@ int main() {
 	// Fireworks
 	std::vector<std::shared_ptr<Firework>> fr;
 	std::vector<bool> done;
+	bool autoFire = false;
 
 	// Text
 	sf::Font font;
@@ -25,6 +27,14 @@ int main() {
 	instr.setFillColor(sf::Color(224, 224, 224));
 	instr.setPosition(sf::Vector2f(350, 30));
 
+	sf::Text toggle;
+	toggle.setFont(font);
+	toggle.setString("Press a to turn on autofire");
+	toggle.setCharacterSize(16);
+	toggle.setFillColor(sf::Color(170, 203, 255));
+	toggle.setPosition(sf::Vector2f(430, 70));
+
+
 	sf::Event event;
 	while (window.isOpen()) {
 		while(window.pollEvent(event)) {
@@ -32,9 +42,10 @@ int main() {
 				case sf::Event::Closed:
 					window.close();
 					instr.setString("");
+					toggle.setString("");
 					break;
 				case sf::Event::KeyPressed:
-					addFireworks(fr, done);
+					addFireworks(fr, done, autoFire, toggle);
 					break;
 				default:
 					break;
@@ -42,14 +53,20 @@ int main() {
 		}
 		window.setFramerateLimit(60);
 
+		// Comment this out for trippy effect
 		window.clear(sf::Color(45, 45, 45));
+
+		// 1 in 60 chance of firing every frame
+		if (autoFire && rand() % 60 == 0) {
+			fire(fr, done);
+		}
 
 		for (int i = 0; i < fr.size(); i++) {
 			if (!done[i]) {
 
 				// If done, delete and mark as done
 				if (fr[i]->execute()) {
-					// TODO: Error: Somehow this code is running even when firework isn't done
+					// Error: this code is running even when firework isn't done
 					// fr.erase(fr.begin() + i);
 					done[i] = true;
 				}
@@ -59,6 +76,7 @@ int main() {
 		}
 
 		window.draw(instr);
+		window.draw(toggle);
 
 		window.display();
 	}
@@ -66,20 +84,33 @@ int main() {
 	return 0;
 }
 
-void addFireworks(std::vector<std::shared_ptr<Firework>>& fr, std::vector<bool>& done) {
+void addFireworks(std::vector<std::shared_ptr<Firework>>& fr, std::vector<bool>& done, bool& autoFire, sf::Text& toggle) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::Space)) {
-		// Release 1-3 fireworks at once
-		for (int i = 0; i < rand() % 3 + 1; i++) {
-			for (int i = 0; i < done.size(); i++) {
-				if (done[i]) {
-					fr[i] = std::make_shared<Firework>();
-					done[i] = false;
-					return;
-				}
-			}
-
-			fr.push_back(std::make_shared<Firework>());
-			done.push_back(false);
+		fire(fr, done);
+	} else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
+		autoFire = !autoFire;
+		if (autoFire) {
+			toggle.setString("Press a to turn off autofire");
+			toggle.setFillColor(sf::Color(255, 170, 170));
+		} else {
+			toggle.setString("Press a to turn on autofire");
+			toggle.setFillColor(sf::Color(170, 203, 255));
 		}
+	}
+}
+
+void fire(std::vector<std::shared_ptr<Firework>>& fr, std::vector<bool>& done) {
+	// Release 1-3 fireworks at once
+	for (int i = 0; i < rand() % 3 + 1; i++) {
+		for (int i = 0; i < done.size(); i++) {
+			if (done[i]) {
+				fr[i] = std::make_shared<Firework>();
+				done[i] = false;
+				return;
+			}
+		}
+
+		fr.push_back(std::make_shared<Firework>());
+		done.push_back(false);
 	}
 }
